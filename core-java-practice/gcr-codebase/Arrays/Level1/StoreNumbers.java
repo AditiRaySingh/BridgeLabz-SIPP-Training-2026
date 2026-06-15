3d308d3b17ce4681c9279bd18032870e3d320c93
aditi ray <aditiraysingh2005@gmail.com>
2026-06-15 19:17:57 +0530

    Added Level 1 and Level 2 array practice programs

---
+++ core-java-practice/gcr-codebase/Arrays/Level1/StoreNumbers.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class StoreNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double[] numbers = new double[10];
+
+        int index = 0;
+
+        while (true) {
+
+            double num = sc.nextDouble();
+
+            if (num <= 0 || index == 10) {
+                break;
+            }
+
+            numbers[index] = num;
+            index++;
+        }
+
+        double total = 0;
+
+        for (int i = 0; i < index; i++) {
+            System.out.println(numbers[i]);
+            total += numbers[i];
+        }
+
+        System.out.println("Sum = " + total);
+    }
+}
