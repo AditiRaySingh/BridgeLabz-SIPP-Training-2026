d4f0f278a393d4e85d404446b90eee57ea45ea61
aditi ray <aditiraysingh2005@gmail.com>
2026-07-15 13:28:52 +0530

    Add Two Pointers and Sliding Window solutions

---
+++ dsa/Two Pointers & Sliding Window/MinimumWindow.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class MinimumWindow {
+
+    public static String minWindowSubstring(String s, String t) {
+
+        if (s.length() == 0 || t.length() == 0)
+            return "";
+
+        Map<Character, Integer> need = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        Map<Character, Integer> window = new HashMap<>();
+
+        int required = need.size();
+        int formed = 0;
+
+        int left = 0;
+        int minLength = Integer.MAX_VALUE;
+        int startIndex = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (need.containsKey(c) &&
+                window.get(c).intValue() == need.get(c).intValue()) {
+                formed++;
+            }
+
+            while (formed == required) {
+
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    startIndex = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                window.put(leftChar, window.get(leftChar) - 1);
+
+                if (need.containsKey(leftChar) &&
+                    window.get(leftChar) < need.get(leftChar)) {
+                    formed--;
+                }
+
+                left++;
+            }
+        }
+
+        return minLength == Integer.MAX_VALUE
+                ? ""
+                : s.substring(startIndex, startIndex + minLength);
+    }
+
+    public static void main(String[] args) {
+
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        System.out.println(minWindowSubstring(s, t));
+    }
+}
---
+++ dsa/Two Pointers & Sliding Window/PairSum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class PairSum {
+
+    public static int[] findPairSum(int[] transactions, int target) {
+        int left = 0;
+        int right = transactions.length - 1;
+
+        while (left < right) {
+            int sum = transactions[left] + transactions[right];
+
+            if (sum == target) {
+                return new int[]{transactions[left], transactions[right]};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 7, 11, 15};
+        int target = 18;
+
+        System.out.println(Arrays.toString(findPairSum(arr, target)));
+    }
+}
